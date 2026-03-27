e6a99630f0455f0fa68e08a6e29bbfdc6d10dce6
Bhargav Bhalodiya <156835982+BhargavBhalodiya04@users.noreply.github.com>
2026-03-27 17:04:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello from Dockerized Node.js app 🚀");
-});
-
-app.listen(PORT, () => {
+// ❗ FIX HERE
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
